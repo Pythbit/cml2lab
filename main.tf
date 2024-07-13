f852cfbce5600230048d32558c1608bc2c36b64e
pythbit <admin@pythbit.com>
2024-07-13 00:39:42 -0700

    Horrible first draft

--- main.tf
+++ main.tf
@@ -28,6 +28,14 @@ data "http" "nb_configs" {
  }
 }
 
+data "http" "nb_cables" {
+ url = "${var.nb_address}/api/dcim/cables/"
+ method = "GET"
+ request_headers = {
+  Authorization = "Token ${var.nb_token}"
+ }
+}
+
 resource "cml2_lab" "lab_labs" {
   for_each = data.netbox_site.nb_sites
   title = each.value["name"]
@@ -40,4 +48,13 @@ resource "cml2_node" "lab_devices" {
   label = each.value["name"]
   nodedefinition = lower(each.value["model"])
   configuration = jsondecode(data.http.nb_configs[each.value["device_id"]].response_body).content
+}
+
+resource "cml2_link" "lab_connections" {
+  count = length(jsondecode(data.http.nb_cables.response_body).results)
+  lab_id = resource.cml2_node.lab_devices["${jsondecode(data.http.nb_cables.response_body).results[count.index].a_terminations[0].object.device.name}"].lab_id
+  node_a = jsondecode(data.http.nb_cables.response_body).results[count.index].a_terminations[0].object.device.name
+  slot_a = split("/",jsondecode(data.http.nb_cables.response_body).results[count.index].a_terminations[0].object.name)[0] == "port" ? "0" : split("/",jsondecode(data.http.nb_cables.response_body).results[count.index].a_terminations[0].object.name)[1]
+  node_b = jsondecode(data.http.nb_cables.response_body).results[count.index].b_terminations[0].object.device.name
+  slot_b = split("/",jsondecode(data.http.nb_cables.response_body).results[count.index].b_terminations[0].object.name)[0] == "port" ? "0" : split("/",jsondecode(data.http.nb_cables.response_body).results[count.index].b_terminations[0].object.name)[1]
 }
--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,18 @@
 #  value = data.netbox_site.nb_sites
 # }
 
+# output "nb_cables" {
+#  value = data.http.nb_cables
+# }
+
 # output "cml_labs" {
 #  value = resource.cml2_lab.lab_labs
 # }
 
 # output "cml_devices" {
 #  value = resource.cml2_node.lab_devices
+# }
+
+# output "cml_links" {
+#  value = resource.cml2_link.lab_connections
 # }
