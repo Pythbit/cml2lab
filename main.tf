11595de43a2f67853367145124ed6fdd509c05b2
pythbit <admin@pythbit.com>
2024-07-14 03:31:47 -0700

    cleanup connection logic, retrieving map values

--- main.tf
+++ main.tf
@@ -38,23 +38,22 @@ data "http" "nb_cables" {
 
 resource "cml2_lab" "lab_labs" {
   for_each = data.netbox_site.nb_sites
-  title = each.value["name"]
-  description = each.value["id"]
+  title = each.value.name
 }
 
 resource "cml2_node" "lab_devices" {
   for_each = {for device in data.netbox_devices.nb_devices.devices: device.name => device}
-  lab_id = resource.cml2_lab.lab_labs[each.value["site_id"]].id
-  label = each.value["name"]
-  nodedefinition = lower(each.value["model"])
-  configuration = jsondecode(data.http.nb_configs[each.value["device_id"]].response_body).content
+  lab_id = resource.cml2_lab.lab_labs[each.value.site_id].id
+  label = each.key
+  nodedefinition = lower(each.value.model)
+  configuration = jsondecode(data.http.nb_configs[each.value.device_id].response_body).content
 }
 
 resource "cml2_link" "lab_connections" {
   for_each = {for k,v in jsondecode(data.http.nb_cables.response_body).results: k => v}
   lab_id = resource.cml2_node.lab_devices[each.value.a_terminations[0].object.device.name].lab_id
   node_a = resource.cml2_node.lab_devices[each.value.a_terminations[0].object.device.name].id
-  slot_a = each.value.a_terminations[0].object.name == "port" ? "0" : split("/",each.value.a_terminations[0].object.name)[1]
+  slot_a = strcontains(each.value.a_terminations[0].object.name, "Ethernet") ? reverse(split("/",each.value.a_terminations[0].object.name))[0] : "0"
   node_b = resource.cml2_node.lab_devices[each.value.b_terminations[0].object.device.name].id
-  slot_b = each.value.b_terminations[0].object.name == "port" ? "0" : split("/",each.value.b_terminations[0].object.name)[1]
+  slot_b = strcontains(each.value.b_terminations[0].object.name, "Ethernet") ? reverse(split("/",each.value.b_terminations[0].object.name))[0] : "0"
 }
