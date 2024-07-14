9dad489e514a862b08445180f3dde634fa421de5
pythbit <admin@pythbit.com>
2024-07-13 20:39:20 -0700

    tflint fixes to terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -8,8 +8,13 @@ terraform {
       source  = "CiscoDevNet/cml2"
       version = "~> 0.7.0"
     }
-    http = {}
+    http = {
+      source = "hashicorp/http"
+      version = "~> 3.4.3"
+    }
   }
+
+  required_version = "~> 1.8.5"
 
   backend "local" {
     path = "terraform.tfstate"
